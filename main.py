b4f21fe73751146aa9a9cfe742f575d62c9fbe75
Anchen-twt <133500238+Anchen-twt@users.noreply.github.com>
2023-07-26 11:32:18 +0800

    添加了存档读档删档功能

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 import random
+import pickle
+import os
 
 from scene import Scene
 from player import Player
 from monster import Monster
 from mechanism import Mechanism
-from mechanism import key_door
 
 class Game:
     def __init__(self):
@@ -43,17 +44,30 @@ class Game:
         self.player = Player(100, 20, [])
  
     def play(self):
+        """启动游戏并处理用户输入"""
         # 提示信息
-        print('操作提示:进入 东 西 南 北 搜刮 退出')
+        print('操作提示:进入 东 西 南 北 搜刮 保存 加载 退出')
         # 显示初始场景信息
         current_scene = self.scenes[self.player.loc]
         print(current_scene.desc())
         while True:
             # 获取玩家输入的操作
             action = input(f'选择操作: ')
-            # 根据操作执行事件                              
+            # 根据操作执行事件
+            if action == '保存':
+                self.save_game('/storage/emulated/0/qpython/冒险文本游戏/data/savegame.dat')
+                print('游戏已保存!')
+            elif action == '加载':
+                game = Game.load_game('/storage/emulated/0/qpython/冒险文本游戏/data/savegame.dat')
+                # 存档存在
+                if game:
+                    game.play()
+                    break 
+            elif action == '清空存档':
+                self.delete_save('/storage/emulated/0/qpython/冒险文本游戏/data/savegame.dat')
+                                             
             # 移动逻辑            
-            if current_scene.validate_movement(action):
+            elif current_scene.validate_movement(action):
                 # 获取下一场景
                 next_scene_name = current_scene.exits[action]   
                 next_scene = self.scenes[next_scene_name]
@@ -100,8 +114,9 @@ class Game:
               print(f"你的最终物品:{self.player.inventory}")
               break
     
-    # 检查游戏是否结束
+    
     def check_game_over(self):
+      """检查游戏是否结束"""
       if '宝藏' in self.player.inventory:
         print("你找到了宝藏,游戏结束!") 
         return True
@@ -110,8 +125,8 @@ class Game:
       else:
         return False
         
-    # 进行战斗
     def fight_with_monster(self, player, monster):
+      """处理玩家和一只怪物之间的战斗"""
       while player.hp and monster.hp > 0:
         # 循环回合制
         monster.attack(player)
@@ -127,8 +142,30 @@ class Game:
         loot = random.choice(monster.drops)
         print(f'你获得了{loot}!')
         player.inventory.append(loot)
-
-
+    
+    def save_game(self, filename):
+        """存档"""
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+    
+    @staticmethod
+    def load_game(filename):
+        """读档"""
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            print('存档不存在!')
+            return None   
+            
+    def delete_save(self, filename):
+        """删档"""
+        if os.path.exists(filename):
+            os.remove(filename)
+            print('存档已清空!')
+        else:
+            print('存档不存在!')
+    
  
 game = Game()
 game.play()
